6181a3a1e3a8bf6fa67d470ba4395112c029a13e
austinbarr@protonmail.com <austinbarr@Nichelles-MacBook-Pro.local>
2018-06-15 08:58:25 -0700

    program has a new object called newPizza

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,16 @@ function Pizza(toppings,size){
   this.price = 20
 };
 
+
 //fronend
 $(function(){
   $("#inputForm").submit(function(event){
     event.preventDefault();
     var inputtedToppings =  $("#toppings").val()
     var inuputtedSize =  $("#size").val()
-    console.log(inputtedToppings)
+
+    newPizza = new Pizza(inputtedToppings, inuputtedSize)
+    console.log(newPizza)
     console.log(inuputtedSize)
   });
 });
